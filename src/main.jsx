05f09587401ae0ef3f9749f59c7b1f5c7170fc8b
notrakib <rakibulhuda.official@gmail.com>
2025-04-19 23:18:08 +0600

    Fixed

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App.jsx";
 
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <PartitionProvider>
     <App />
   </PartitionProvider>
